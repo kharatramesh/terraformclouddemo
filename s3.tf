4b2a35658b170322e838d4dad6f0c91a7effa3dc
trainer <trainer@gmail.com>
2025-04-09 17:01:16 +0530

    3

--- s3.tf
+++ s3.tf
@@ -1,6 +1,9 @@
-resource "aws_s3_bucket" "b1" {
-  bucket = "trainer-test-bucket-1234567890"
+resource "aws_s3_bucket" "b3" {
+  bucket = "trainer3-test-bucket-1234567890"
 }
 resource "aws_s3_bucket" "b2" {
   bucket = "trainer1-test-bucket-1234567890"
+}
+resource "aws_s3_bucket" "b4" {
+  bucket = "trainer4-test-bucket-1234567890"
 }
